ntudlclearntech/canvas-lms

commit 8d5eb3cb764eaa1cc8019f9856b665ad7105147c

graded icon updates when SpeedGrader's scores are updated
fixes CNVS-23976

This regression was introduced by 12f6da33.

test plan:
 0. As a teacher, create a quiz with an essay question.
 1. As a student, take the quiz.
 2. As the teacher, go to the SpeedGrader.
 3. Enter a score for the essay quiz question.
 4. Click the "Update Scores" button.
 5. Verify the orange dot next to the students name becomes
    a green check mark.
 6. Delete the score for the essay quiz question.
 7. Click the "Update Scores" button.
 8. Verify the green check mark next to the students name
    becomes an orange dot.

Change-Id: I2b7b1ce787375f60fb3c8967b520049108bf5edf
Reviewed-on: https://gerrit.instructure.com/67016
Tested-by: Jenkins
Reviewed-by: Keith T. Garner <kgarner@instructure.com>
QA-Review: Jason Carter <jcarter@instructure.com>
Product-Review: Keith T. Garner <kgarner@instructure.com>

public/javascripts/speed_grader.js
@@ -291,8 +291,9 @@ define([
   }
 
   function classNameBasedOnStudent(student){
+    var raw       = submissionState(student);
     var formatted = formattedsubmissionState(student.submission_state, student.submission);
-    return {raw: student.submission_state, formatted: formatted};
+    return {raw: raw, formatted: formatted};
   }
 
   // xsslint safeString.identifier MENU_PARTS_DELIMITER

commit 26577a1cab8f4f2af62c6500fa44f5610371e156

fix moderated grading submission states
This regression was introduced by 8d5eb3cb7

test plan:
* create a submission for a moderated assignment
* as a ta, add a provisional grade
* add the user to the moderation set
* the moderator should not see the submission as already "graded"
 (since it needs another provisional grade)

clsoes #CNVS-25722

Change-Id: I8bf7476b9f731859ee5a2f4563c63614db9684db
Reviewed-on: https://gerrit.instructure.com/68669
Tested-by: Jenkins
Reviewed-by: Jeremy Stanley <jeremy@instructure.com>
QA-Review: Jahnavi Yetukuri <jyetukuri@instructure.com>
Product-Review: James Williams  <jamesw@instructure.com>

public/javascripts/speed_grader.js
@@ -295,7 +295,7 @@ define([
   }
 
   function classNameBasedOnStudent(student){
-    var raw       = submissionState(student);
+    var raw       = student.submission_state;
     var formatted = formattedsubmissionState(student.submission_state, student.submission);
     return {raw: raw, formatted: formatted};
   }
@@ -820,6 +820,7 @@ define([
         function(data){
           if(currentStudentIDAsOfAjaxCall === EG.currentStudent.id) {
             EG.currentStudent.submission = data.submission;
+            EG.currentStudent.submission_state = submissionState(EG.currentStudent);
             EG.showGrade();
           }
       });

spec/selenium/speedgrader_moderated_grading_spec.rb
@@ -175,6 +175,8 @@ describe "speed grader" do
       @assignment.moderated_grading_selections.create!(:student => @student)
 
       get "/courses/#{@course.id}/gradebook/speed_grader?assignment_id=#{@assignment.id}"
+      expect(f("#students_selectmenu-button")).to have_class("not_graded")
+      
       new_mark_dd = f('#moderation_bar #new_mark_dropdown_link')
       expect(new_mark_dd).to include_text("Add Review")
       new_mark_dd.click

spec/selenium/speedgrader_teacher_spec.rb
@@ -346,12 +346,16 @@ describe "speed grader" do
     driver.switch_to.frame f('#speedgrader_iframe')
     points_input = ff('#questions .user_points input')
     driver.execute_script("$('#questions .user_points input').focus()")
+    replace_content(points_input[0], '0')
     replace_content(points_input[1], '.5')
+    replace_content(points_input[2], '0')
     f('.update_scores button[type="submit"]').click
     wait_for_ajaximations
     # Switch to the right panel
     # Verify that the grade is .5
     driver.switch_to.default_content
     expect(f('#grading-box-extended')['value']).to eq('0.5')
+    expect(f("#students_selectmenu-button")).to_not have_class("not_graded")
+    expect(f("#students_selectmenu-button")).to have_class("graded")
   end
 end