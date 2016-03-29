ntudlclearntech/canvas-lms

commit 793e55a6275f921a558828dd53b13203fb8f8912

don't show 'Correct Answer' tag for graded surveys
when a teacher is viewing submissions for a graded
survey in speedgrader, there should no longer be a
'Correct Answer' flag showing for multiple choice
questions.

closes CNVS-27084

test plan:
1) Create a new quiz with the type 'Graded Survey'
2) Add a multiple choice question to the survey
3) Take the survey as a student
4) As a teacher, view the student's submission in
   Speedgrader.
5) Verify there is no 'Correct Answer' flag next to
   any of the radio buttons on the multiple choice
   question
6) Repeat steps 1-4 but make the quiz with a type
   of 'Graded Quiz'. Verify there _is_ a 'Correct
   Answer' flag on multiple choice questions.

Change-Id: Id5fe779a476d6602d8dbade88e2167f1ab99f31d
Reviewed-on: https://gerrit.instructure.com/74406
Tested-by: Jenkins
Reviewed-by: Ryan Taylor <rtaylor@instructure.com>
QA-Review: KC Naegle <knaegle@instructure.com>
Product-Review: Spencer Olson <solson@instructure.com>

public/javascripts/quiz_arrows.js
@@ -18,65 +18,71 @@
 
 // xsslint jqueryObject.identifier /Tpl$/
 
-define(['i18n!quizzes.show', 'jquery'], function(I18n, $) {
+define(['i18n!quizzes.show', 'jquery', 'str/htmlEscape'], function(I18n, $, htmlEscape) {
   // Create and append right/wrong arrows to all appropriate
   // answers on a quiz results page.
-  return function() {
-    var $questions = $('#questions.show_correct_answers:not(.survey_quiz)');
-    var rightAnswers     = $questions.find('.selected_answer.correct_answer'),
-        wrongAnswers     = $questions.find('.selected_answer.wrong_answer'),
-        correctAnswers   = $questions.find('.question:not(.short_answer_question, .numerical_question, .matching_question) .correct_answer:not(.selected_answer)'),
-        editableMatches  = $('#quiz_edit_wrapper').find($questions.selector).find('.question.matching_question .correct_answer:not(.selected_answer)'),
-        readOnlyMatches  = $('#quiz_show').find($questions.selector).find('.question.matching_question .correct_answer:not(.selected_answer)'),
-        shortAnswers     = $questions.filter(':not(.survey_results)').find('.short_answer_question .answers_wrapper, #questions.show_correct_answers:not(.survey_results):not(.survey_quiz) .numerical_question .answers_wrapper, #questions.show_correct_answers:not(.survey_results):not(.survey_quiz) .equation_combinations_holder_holder.calculated_question_answers'),
-        unansweredQ      = $('.question.unanswered .header .question_name'),
-        creditPartial    = $('#questions.suppress_correct_answers:not(.survey_results) .question.partial_credit .header .question_name'),
-        creditFull       = $('#questions.suppress_correct_answers:not(.survey_results) .question.correct .header .question_name'),
-        creditNone       = $('#questions.suppress_correct_answers:not(.survey_results) .question.incorrect:not(.unanswered) .header .question_name'),
-        surveyAnswers    = $('#questions.survey_results .selected_answer'),
-        rightTpl         = $('<span />', { 'class': 'answer_arrow correct' }),
-        wrongTpl         = $('<span />', { 'class': 'answer_arrow incorrect' }),
-        correctTpl       = $('<span />', { 'class': 'answer_arrow info' }),
-        shortTpl         = $('<span />', { 'class': 'answer_arrow info' }),
-        unansweredTpl    = $('<span />', { 'class': 'answer_arrow incorrect' }),
-        creditFullTpl    = $('<span />', { 'class': 'answer_arrow correct' }),
-        creditPartialTpl = $('<span />', { 'class': 'answer_arrow incorrect' }),
-        creditNoneTpl    = $('<span />', { 'class': 'answer_arrow incorrect' }),
-        surveyAnswerTpl  = $('<span />', { 'class': 'answer_arrow info' }),
-        idGenerator      = 0;
+  function QuizArrowApplicator() {
+    this.$questions       = $('#questions.show_correct_answers:not(.survey_quiz)');
+    this.rightAnswers     = this.$questions.find('.selected_answer.correct_answer');
+    this.wrongAnswers     = this.$questions.find('.selected_answer.wrong_answer');
+    this.correctAnswers   = this.$questions.find('.question:not(.short_answer_question, .numerical_question, .matching_question) .correct_answer:not(.selected_answer)');
+    this.editableMatches  = $('#quiz_edit_wrapper').find(this.$questions.selector).find('.question.matching_question .correct_answer:not(.selected_answer)');
+    this.readOnlyMatches  = $('#quiz_show').find(this.$questions.selector).find('.question.matching_question .correct_answer:not(.selected_answer)');
+    this.shortAnswers     = this.$questions.filter(':not(.survey_results)').find('.short_answer_question .answers_wrapper, #questions.show_correct_answers:not(.survey_results):not(.survey_quiz) .numerical_question .answers_wrapper, #questions.show_correct_answers:not(.survey_results):not(.survey_quiz) .equation_combinations_holder_holder.calculated_question_answers');
+    this.unansweredQ      = $('.question.unanswered .header .question_name');
+    this.creditPartial    = $('#questions.suppress_correct_answers:not(.survey_results) .question.partial_credit .header .question_name');
+    this.creditFull       = $('#questions.suppress_correct_answers:not(.survey_results) .question.correct .header .question_name');
+    this.creditNone       = $('#questions.suppress_correct_answers:not(.survey_results) .question.incorrect:not(.unanswered) .header .question_name');
+    this.surveyAnswers    = $('#questions.survey_results .selected_answer');
+    this.rightTpl         = $('<span />', { 'class': 'answer_arrow correct' });
+    this.wrongTpl         = $('<span />', { 'class': 'answer_arrow incorrect' });
+    this.correctTpl       = $('<span />', { 'class': 'answer_arrow info' });
+    this.shortTpl         = $('<span />', { 'class': 'answer_arrow info' });
+    this.unansweredTpl    = $('<span />', { 'class': 'answer_arrow incorrect' });
+    this.creditFullTpl    = $('<span />', { 'class': 'answer_arrow correct' });
+    this.creditPartialTpl = $('<span />', { 'class': 'answer_arrow incorrect' });
+    this.creditNoneTpl    = $('<span />', { 'class': 'answer_arrow incorrect' });
+    this.surveyAnswerTpl  = $('<span />', { 'class': 'answer_arrow info' });
+    this.idGenerator      = 0;
+  }
 
-    $.each([rightTpl, wrongTpl, correctTpl, shortTpl, surveyAnswerTpl], function() {
+  QuizArrowApplicator.prototype.applyCSS = function() {
+    $.each([this.rightTpl, this.wrongTpl, this.correctTpl, this.shortTpl, this.surveyAnswerTpl], function() {
       this.css({ left: -128, top: 5 });
     });
-    $.each([unansweredTpl, creditFullTpl, creditNoneTpl, creditPartialTpl], function() {
+    $.each([this.unansweredTpl, this.creditFullTpl, this.creditNoneTpl, this.creditPartialTpl], function() {
       this.css({ left: -108, top: 9 });
     });
+  }
 
+  QuizArrowApplicator.prototype.applyCorrectAndIncorrectArrows = function() {
+    this.rightTpl.text(I18n.t('answers.correct', 'Correct!'));
+    this.wrongTpl.text(I18n.t('answers.you_answered', 'You Answered'));
+    this.correctTpl.text(I18n.t('answers.right', 'Correct Answer'));
+    this.shortTpl.text(I18n.t('answers.correct_answers', 'Correct Answers'));
+    this.creditFullTpl.text(I18n.t('answers.correct', 'Correct!'));
+    this.creditPartialTpl.text(I18n.t('answers.partial', 'Partial'));
+    this.creditNoneTpl.text(I18n.t('answers.incorrect', 'Incorrect'));
 
-    rightTpl.text(I18n.t('answers.correct', 'Correct!'));
-    wrongTpl.text(I18n.t('answers.you_answered', 'You Answered'));
-    correctTpl.text(I18n.t('answers.right', 'Correct Answer'));
-    shortTpl.text(I18n.t('answers.correct_answers', 'Correct Answers'));
-    unansweredTpl.text(I18n.t('answers.unanswered', 'Unanswered'));
-    creditFullTpl.text(I18n.t('answers.correct', 'Correct!'));
-    creditPartialTpl.text(I18n.t('answers.partial', 'Partial'));
-    creditNoneTpl.text(I18n.t('answers.incorrect', 'Incorrect'));
-    surveyAnswerTpl.text(I18n.t('answers.you_answered', 'You Answered'));
+    this.rightAnswers.prepend(htmlEscape(this.rightTpl));
+    this.wrongAnswers.prepend(htmlEscape(this.wrongTpl));
+    this.correctAnswers.prepend(htmlEscape(this.correctTpl));
+    this.editableMatches.parent().before(htmlEscape(this.correctTpl));
+    this.readOnlyMatches.prepend(htmlEscape(this.correctTpl));
+    this.shortAnswers.prepend(htmlEscape(this.shortTpl));
+    this.creditPartial.prepend(htmlEscape(this.creditPartialTpl));
+    this.creditFull.prepend(htmlEscape(this.creditFullTpl));
+    this.creditNone.prepend(htmlEscape(this.creditNoneTpl));
+  }
 
-    if(!ENV.IS_SURVEY){
-      rightAnswers.prepend(rightTpl);
-      wrongAnswers.prepend(wrongTpl);
-      correctAnswers.prepend(correctTpl);
-      editableMatches.parent().before(correctTpl);
-      readOnlyMatches.prepend(correctTpl);
-      shortAnswers.prepend(shortTpl);
-      creditPartial.prepend(creditPartialTpl);
-      creditFull.prepend(creditFullTpl);
-      creditNone.prepend(creditNoneTpl);
-    }
-    unansweredQ.prepend(unansweredTpl);
-    surveyAnswers.prepend(surveyAnswerTpl);
+  QuizArrowApplicator.prototype.applyAnsweredAndUnansweredArrows = function() {
+    this.unansweredTpl.text(I18n.t('answers.unanswered', 'Unanswered'));
+    this.surveyAnswerTpl.text(I18n.t('answers.you_answered', 'You Answered'));
+    this.unansweredQ.prepend(htmlEscape(this.unansweredTpl));
+    this.surveyAnswers.prepend(htmlEscape(this.surveyAnswerTpl));
+  }
 
+  QuizArrowApplicator.prototype.makeArrowsAccessible = function() {
     // adjust these down a little so they align better w/ answers.
     $('.short_answer_question .answer_arrow').css('top', 5);
 
@@ -133,5 +139,14 @@ define(['i18n!quizzes.show', 'jquery'], function(I18n, $) {
       // got above.
       $target.attr('aria-describedby', arrowId);
     });
-  };
+  }
+
+  QuizArrowApplicator.prototype.applyArrows = function() {
+    this.applyCSS();
+    if (!ENV.IS_SURVEY) this.applyCorrectAndIncorrectArrows();
+    this.applyAnsweredAndUnansweredArrows();
+    this.makeArrowsAccessible();
+  }
+
+  return QuizArrowApplicator;
 });

app/coffeescripts/bundles/quiz_history.coffee
@@ -3,8 +3,9 @@ require [
   'quiz_arrows'
   'quiz_inputs'
   'quiz_history'
-], ($, createQuizArrows, inputMethods) ->
+], ($, QuizArrowApplicator, inputMethods) ->
   $ ->
-    createQuizArrows()
+    arrowApplicator = new QuizArrowApplicator()
+    arrowApplicator.applyArrows()
     inputMethods.disableInputs('[type=radio], [type=checkbox]')
     inputMethods.setWidths()

app/coffeescripts/bundles/quizzes_bundle.coffee
@@ -4,12 +4,13 @@ require [
   'quizzes'
   'supercalc'
   'quiz_rubric'
-], ($, CreateQuizArrows) ->
+], ($, QuizArrowApplicator) ->
 
   $('#show_question_details').on 'click', (e)->
     # Create the quiz arrows
     if $(this).is(':checked')
-      CreateQuizArrows()
+      arrowApplicator = new QuizArrowApplicator()
+      arrowApplicator.applyArrows()
     else
       # Delete all quiz arrows
       $('.answer_arrow').remove()
@@ -19,4 +20,5 @@ require [
     # Remove all arrows and recreate all if option is checked
     $('.answer_arrow').remove()
     if $('#show_question_details').is(':checked')
-        CreateQuizArrows()
+      arrowApplicator = new QuizArrowApplicator()
+      arrowApplicator.applyArrows()

app/controllers/quizzes/quizzes_controller.rb
@@ -210,13 +210,14 @@ class Quizzes::QuizzesController < ApplicationController
       submission_counts if @quiz.grants_right?(@current_user, session, :grade) || @quiz.grants_right?(@current_user, session, :read_statistics)
       @stored_params = (@submission.temporary_data rescue nil) if params[:take] && @submission && (@submission.untaken? || @submission.preview?)
       @stored_params ||= {}
-      hash = { :QUIZZES_URL => course_quizzes_url(@context),
-             :IS_SURVEY => @quiz.survey?,
-             :QUIZ => quiz_json(@quiz,@context,@current_user,session),
-             :COURSE_ID => @context.id,
-             :LOCKDOWN_BROWSER => @quiz.require_lockdown_browser?,
-             :ATTACHMENTS => Hash[@attachments.map { |_,a| [a.id,attachment_hash(a)]}],
-             :CONTEXT_ACTION_SOURCE => :quizzes  }
+      hash = {
+        ATTACHMENTS: Hash[@attachments.map { |_,a| [a.id,attachment_hash(a)]}],
+        CONTEXT_ACTION_SOURCE: :quizzes,
+        COURSE_ID: @context.id,
+        LOCKDOWN_BROWSER: @quiz.require_lockdown_browser?,
+        QUIZ: quiz_json(@quiz,@context,@current_user,session),
+        QUIZZES_URL: course_quizzes_url(@context)
+      }
       append_sis_data(hash)
       js_env(hash)
 

app/views/quizzes/quizzes/_quiz_submission.html.erb
@@ -3,6 +3,7 @@
   <h4><%= render_result_protection(@quiz, @current_submission || @submission) %></h4>
 <% end %>
 
+<% js_env IS_SURVEY: @quiz.survey? %>
 <% unless show_correct_answers? %>
   <div class="alert">
     <i class="icon-warning" aria-label="<%= render_correct_answer_protection(@quiz, (@current_submission || @submission)) %>"></i>

public/javascripts/quiz_show.js
@@ -33,7 +33,7 @@ define([
   'jquery.instructure_misc_plugins' /* ifExists, confirmDelete */,
   'jquery.disableWhileLoading',
   'message_students' /* messageStudents */
-], function(I18n, $, MessageStudentsDialog, showAnswerArrows, inputMethods, Quiz, PublishButtonView, QuizLogAuditingEventDumper) {
+], function(I18n, $, MessageStudentsDialog, QuizArrowApplicator, inputMethods, Quiz, PublishButtonView, QuizLogAuditingEventDumper) {
 
 
   $(document).ready(function () {
@@ -56,7 +56,8 @@ define([
       };
     }
 
-    showAnswerArrows();
+    var arrowApplicator = new QuizArrowApplicator();
+    arrowApplicator.applyArrows();
     // quiz_show is being pulled into ember show for now. only hide inputs
     // when we don't have a .allow-inputs
     if (!$('.allow-inputs').length) {

spec/coffeescripts/quizzes/QuizArrowsSpec.coffee
@@ -1,0 +1,24 @@
+define [
+  'quiz_arrows'
+  'helpers/fakeENV'
+], (QuizArrowApplicator, fakeENV) ->
+
+  module 'QuizArrowApplicator',
+    setup: ->
+      fakeENV.setup()
+      @arrowApplicator = new QuizArrowApplicator()
+
+    teardown: ->
+      fakeENV.teardown()
+
+  test "applies 'correct' and 'incorrect' arrows when the quiz is not a survey", ->
+    @spy(@arrowApplicator, 'applyCorrectAndIncorrectArrows')
+    ENV.IS_SURVEY = false
+    @arrowApplicator.applyArrows()
+    ok @arrowApplicator.applyCorrectAndIncorrectArrows.calledOnce
+
+  test "does not apply 'correct' and 'incorrect' arrows when the quiz is a survey", ->
+    @spy(@arrowApplicator, 'applyCorrectAndIncorrectArrows')
+    ENV.IS_SURVEY = true
+    @arrowApplicator.applyArrows()
+    ok @arrowApplicator.applyCorrectAndIncorrectArrows.notCalled

spec/views/quizzes/quizzes/_quiz_submission.html.erb_spec.rb
@@ -16,32 +16,46 @@
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 
-require File.expand_path(File.dirname(__FILE__) + '/../../../spec_helper')
-require File.expand_path(File.dirname(__FILE__) + '/../../views_helper')
+require_relative '../../../spec_helper'
+require_relative '../../views_helper'
 
 describe "/quizzes/quizzes/_quiz_submission" do
-  it "should render" do
+  before(:each) do
     course_with_student
     view_context
-    assigns[:quiz] = @course.quizzes.create!
-    assigns[:submission] = assigns[:quiz].generate_submission(@user)
-    Quizzes::SubmissionGrader.new(assigns[:submission]).grade_submission
-    render :partial => "quizzes/quizzes/quiz_submission"
-    expect(response).not_to be_nil
   end
 
-  it "should render when quiz results are not supposed to be shown to the student" do
-    course_with_student
-    view_context
-    quiz = @course.quizzes.create!
-    quiz.hide_results = 'always'
-    quiz.save!
+  context "quiz results are visible to the student" do
+    before(:each) do
+      quiz = @course.quizzes.create!
+      submission = quiz.generate_submission(@user)
+      assigns[:quiz] = quiz
+      assigns[:submission] = submission
+      Quizzes::SubmissionGrader.new(submission).grade_submission
+    end
 
-    assigns[:quiz] = quiz
-    assigns[:submission] = assigns[:quiz].generate_submission(@user)
-    Quizzes::SubmissionGrader.new(assigns[:submission]).grade_submission
-    render :partial => "quizzes/quizzes/quiz_submission"
-    expect(response).not_to be_nil
+    it "renders" do
+      render :partial => "quizzes/quizzes/quiz_submission"
+      expect(response).not_to be_nil
+    end
+
+    it "sets the IS_SURVEY value in the js env" do
+      render :partial => "quizzes/quizzes/quiz_submission"
+      expect(controller.js_env.key?(:IS_SURVEY)).to eq(true)
+    end
+  end
+
+  context "quiz results are not visible to the student" do
+    it "renders" do
+      quiz = @course.quizzes.create!
+      quiz.hide_results = 'always'
+      quiz.save!
+
+      assigns[:quiz] = quiz
+      assigns[:submission] = assigns[:quiz].generate_submission(@user)
+      Quizzes::SubmissionGrader.new(assigns[:submission]).grade_submission
+      render :partial => "quizzes/quizzes/quiz_submission"
+      expect(response).not_to be_nil
+    end
   end
 end
-

commit 2d3c1d9e7a96d9e733b0d57edf924dbe0887387c

fix arrows for user-generated incorrect answers
fix a regression in 793e55a. this fix will make
`rspec ./spec/selenium/quizzes/quizzes_teacher_questions_spec.rb:16`
stop failing (it currently fails once on most builds)

also remove unnecessary html-escaping introduced in that commit

test plan:
1) Create a new quiz with the type 'Graded Survey'
2) Add a multiple choice question to the survey
3) Take the survey as a student
4) As a teacher, view the student's submission in
   Speedgrader.
5) Verify there is no 'Correct Answer' flag next to
   any of the radio buttons on the multiple choice
   question
6) Repeat steps 1-4 but make the quiz with a type
   of 'Graded Quiz'. Also make sure that you select
   an _incorrect_ answer when taking the quiz. Verify
   there _is_ a 'Correct Answer' flag and an 'Incorrect
   'Answer' flag on multiple choice questions.

Change-Id: I9c9cd9ad8d9deabc9484efb8928a306305750280
Reviewed-on: https://gerrit.instructure.com/75587
Reviewed-by: Spencer Olson <solson@instructure.com>
Tested-by: Jenkins
QA-Review: KC Naegle <knaegle@instructure.com>
Product-Review: Jon Jensen <jon@instructure.com>

public/javascripts/quiz_arrows.js
@@ -16,9 +16,11 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-// xsslint jqueryObject.identifier /Tpl$/
+// xsslint jqueryObject.property /Tpl$/
 
-define(['i18n!quizzes.show', 'jquery', 'str/htmlEscape'], function(I18n, $, htmlEscape) {
+define(['i18n!quizzes.show', 'jquery'], function(I18n, $) {
+  var idGenerator = 0;
+
   // Create and append right/wrong arrows to all appropriate
   // answers on a quiz results page.
   function QuizArrowApplicator() {
@@ -43,7 +45,6 @@ define(['i18n!quizzes.show', 'jquery', 'str/htmlEscape'], function(I18n, $, html
     this.creditPartialTpl = $('<span />', { 'class': 'answer_arrow incorrect' });
     this.creditNoneTpl    = $('<span />', { 'class': 'answer_arrow incorrect' });
     this.surveyAnswerTpl  = $('<span />', { 'class': 'answer_arrow info' });
-    this.idGenerator      = 0;
   }
 
   QuizArrowApplicator.prototype.applyCSS = function() {
@@ -64,22 +65,22 @@ define(['i18n!quizzes.show', 'jquery', 'str/htmlEscape'], function(I18n, $, html
     this.creditPartialTpl.text(I18n.t('answers.partial', 'Partial'));
     this.creditNoneTpl.text(I18n.t('answers.incorrect', 'Incorrect'));
 
-    this.rightAnswers.prepend(htmlEscape(this.rightTpl));
-    this.wrongAnswers.prepend(htmlEscape(this.wrongTpl));
-    this.correctAnswers.prepend(htmlEscape(this.correctTpl));
-    this.editableMatches.parent().before(htmlEscape(this.correctTpl));
-    this.readOnlyMatches.prepend(htmlEscape(this.correctTpl));
-    this.shortAnswers.prepend(htmlEscape(this.shortTpl));
-    this.creditPartial.prepend(htmlEscape(this.creditPartialTpl));
-    this.creditFull.prepend(htmlEscape(this.creditFullTpl));
-    this.creditNone.prepend(htmlEscape(this.creditNoneTpl));
+    this.rightAnswers.prepend(this.rightTpl);
+    this.wrongAnswers.prepend(this.wrongTpl);
+    this.correctAnswers.prepend(this.correctTpl);
+    this.editableMatches.parent().before(this.correctTpl);
+    this.readOnlyMatches.prepend(this.correctTpl);
+    this.shortAnswers.prepend(this.shortTpl);
+    this.creditPartial.prepend(this.creditPartialTpl);
+    this.creditFull.prepend(this.creditFullTpl);
+    this.creditNone.prepend(this.creditNoneTpl);
   }
 
   QuizArrowApplicator.prototype.applyAnsweredAndUnansweredArrows = function() {
     this.unansweredTpl.text(I18n.t('answers.unanswered', 'Unanswered'));
     this.surveyAnswerTpl.text(I18n.t('answers.you_answered', 'You Answered'));
-    this.unansweredQ.prepend(htmlEscape(this.unansweredTpl));
-    this.surveyAnswers.prepend(htmlEscape(this.surveyAnswerTpl));
+    this.unansweredQ.prepend(this.unansweredTpl);
+    this.surveyAnswers.prepend(this.surveyAnswerTpl);
   }
 
   QuizArrowApplicator.prototype.makeArrowsAccessible = function() {