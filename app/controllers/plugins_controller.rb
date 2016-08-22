ntudlclearntech/canvas-lms

commit 20dd19be66360ce09782c77aa605586a3a7e97ef

disable new plugin settings when creating them, not on display
refs CNVS-30864

also be tolerant of plugin settings change from a, uh, plugin

Change-Id: Ib61280fc79ea57a3791092e3a36273f1ccbcaa95
Reviewed-on: https://gerrit.instructure.com/38462
Tested-by: Jenkins
Reviewed-by: Simon Williams <simon@instructure.com>
Product-Review: Cody Cutrer <cody@instructure.com>
QA-Review: Cody Cutrer <cody@instructure.com>

app/controllers/plugins_controller.rb
@@ -27,7 +27,6 @@ class PluginsController < ApplicationController
   def show
     if find_plugin_setting
       if @plugin_setting.new_record?
-        @plugin_setting.disabled = true
         clear_encrypted_plugin_settings
       end
       @settings = @plugin.settings
@@ -60,7 +59,7 @@ class PluginsController < ApplicationController
   def find_plugin_setting
     if @plugin = Canvas::Plugin.find(params[:id])
       @plugin_setting = PluginSetting.find_by_name(@plugin.id)
-      @plugin_setting ||= PluginSetting.new(:name => @plugin.id, :settings => @plugin.default_settings)
+      @plugin_setting ||= PluginSetting.new(:name => @plugin.id, :settings => @plugin.default_setting) { |ps| ps.disabled = true }
       true
     else
       false

spec/selenium/default_plugins_spec.rb
@@ -98,6 +98,9 @@ describe "default plugins" do
       f("#accounts_select option:nth-child(2)").click
       expect(f("#plugin_setting_disabled")).to be_displayed
     end
+    if !f(".save_button").enabled?
+      f(".copy_settings_button").click
+    end
   end
 end
 

spec/selenium/plugins_spec.rb
@@ -61,7 +61,9 @@ describe "plugins ui" do
       f("#accounts_select option:nth-child(2)").click
       expect(f("#plugin_setting_disabled")).to be_displayed
     end
+    if !f(".save_button").enabled?
+      f(".copy_settings_button").click
+    end
   end
-
 end
 

commit 24652d30db8c9bfcee87f9bd8c5041e6d7b01411

fix typo in plugin default settings
introduced by 20dd19be66360ce09782c77aa605586a3a7e97ef

Change-Id: Ib1f560abbee67eb9c8619002252f0c32264c4a0d
Reviewed-on: https://gerrit.instructure.com/88417
Tested-by: Jenkins
Reviewed-by: Ben Young <byoung@instructure.com>
Product-Review: Cody Cutrer <cody@instructure.com>
QA-Review: Cody Cutrer <cody@instructure.com>

app/controllers/plugins_controller.rb
@@ -59,7 +59,7 @@ class PluginsController < ApplicationController
   def find_plugin_setting
     if @plugin = Canvas::Plugin.find(params[:id])
       @plugin_setting = PluginSetting.find_by_name(@plugin.id)
-      @plugin_setting ||= PluginSetting.new(:name => @plugin.id, :settings => @plugin.default_setting) { |ps| ps.disabled = true }
+      @plugin_setting ||= PluginSetting.new(:name => @plugin.id, :settings => @plugin.default_settings) { |ps| ps.disabled = true }
       true
     else
       false