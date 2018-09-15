ntudlclearntech/canvas-lms

commit cfe2ce6b1a0662ff464aa4c4249ff450118f7002

Show provisional grader rubrics to moderator
For a moderated assignment, allow the moderator to view rubrics
submitted by provisional graders and enter a custom assessment.

fixes GRADE-1377

Test plan:
  - Set up a rubric
  - Set up two moderated assignments, one with grader names visible to
    the final grader and the other with them not visible
      - Attach the rubric to each assignment
  - As one or more provisional graders, assess some students using the
    rubric in each assignment
    - This should function as normal
  - As the moderator, open each assignment in SpeedGrader
    - The right side should have a dropdown letting you select among
      provisional graders' submitted assessments
      - If grader names are visible, the dropdown options will show that
        provisional grader's name
      - If not, they will appear as "Grader # Rubric" (note the "Rubric"
        suffix for anonymous graders), numbered as elsewhere on the page
      - A "Custom" option representing the moderator's assessment should
        always be present, and be the first option
        - The button to edit the rubric should only appear when this
          option is selected
    - Note that assessment scores are not (as of the time of writing)
      automatically propagated to provisional grades, so the scores
      shown in the grade selector will not necessarily reflect the
      contents of the rubric

Change-Id: I3cc05bc64b687096638c64f02633b37484a63455
Reviewed-on: https://gerrit.instructure.com/161822
Tested-by: Jenkins
Reviewed-by: Gary Mei <gmei@instructure.com>
QA-Review: Gary Mei <gmei@instructure.com>
Reviewed-by: Keith T. Garner <kgarner@instructure.com>
Product-Review: Sidharth Oberoi <soberoi@instructure.com>

public/javascripts/speed_grader.js
@@ -49,6 +49,7 @@ import SpeedgraderHelpers, {
   setupAnonymizableStudentId,
   setupAnonymizableUserId,
   setupAnonymizableAuthorId,
+  setupAnonymousGraders,
   setupIsAnonymous
 } from './speed_grader_helpers';
 import turnitinInfoTemplate from 'jst/_turnitinInfo';
@@ -81,6 +82,7 @@ const SPEED_GRADER_SUBMISSION_COMMENTS_DOWNLOAD_MOUNT_POINT = 'speed_grader_subm
 const SPEED_GRADER_SETTINGS_MOUNT_POINT = 'speed_grader_settings_mount_point';
 
 let isAnonymous
+let anonymousGraders
 let anonymizableId
 let anonymizableUserId
 let anonymizableStudentId
@@ -803,6 +805,18 @@ function getSelectedAssessment(){
   ))[0];
 }
 
+function assessmentBelongsToCurrentUser(assessment) {
+  if (!assessment) {
+    return false
+  }
+
+  if (anonymousGraders) {
+    return ENV.current_anonymous_id === assessment.anonymous_assessor_id
+  } else {
+    return ENV.current_user_id === assessment.assessor_id
+  }
+}
+
 function handleSelectedRubricAssessmentChanged({validateEnteredData = true} = {}) {
   // This function is triggered both when we assess a student and when we switch
   // students. In the former case, we want populateNewRubricSummary to check the
@@ -812,12 +826,16 @@ function handleSelectedRubricAssessmentChanged({validateEnteredData = true} = {}
   // data is switched over to the new student, we don't want to perform the
   // comparison since it could result in specious alerts being shown.
   const editingData = validateEnteredData ? rubricAssessment.assessmentData($("#rubric_full")) : null
+  const selectedAssessment = getSelectedAssessment()
   rubricAssessment.populateNewRubricSummary(
     $("#rubric_summary_holder .rubric_summary"),
-    getSelectedAssessment(),
+    selectedAssessment,
     jsonData.rubric_association,
     editingData
   );
+
+  const showEditButton = !selectedAssessment || assessmentBelongsToCurrentUser(selectedAssessment)
+  $('#rubric_assessments_list_and_edit_button_holder .edit').showIf(showEditButton)
 }
 
 function initRubricStuff(){
@@ -1140,6 +1158,7 @@ EG = {
 
   jsonReady: function(){
     isAnonymous = setupIsAnonymous(jsonData)
+    anonymousGraders = setupAnonymousGraders(jsonData)
     anonymizableId = setupAnonymizableId(isAnonymous)
     anonymizableUserId = setupAnonymizableUserId(isAnonymous)
     anonymizableStudentId = setupAnonymizableStudentId(isAnonymous)
@@ -1334,6 +1353,49 @@ EG = {
     }
   },
 
+  setCurrentStudentRubricAssessments () {
+    // currentStudent.rubric_assessments only includes assessments submitted
+    // by the current user, so if the viewer is a moderator, get other
+    // graders' assessments from their provisional grades.
+    const provisionalAssessments = []
+
+    // If the moderator has just saved a new assessment, this array will have
+    // entries not present elsewhere, so don't clobber them.
+    const currentAssessmentsById = {}
+    if (this.currentStudent.rubric_assessments) {
+      this.currentStudent.rubric_assessments.forEach(assessment => {
+        currentAssessmentsById[assessment.id] = true
+      })
+    }
+
+    currentStudentProvisionalGrades().forEach(grade => {
+      // TODO: decide what to do if a provisional grade contains multiple
+      // assessments (currently we're not sure if this can actually happen
+      // for a moderated assignment).
+      if (grade.rubric_assessments && grade.rubric_assessments.length > 0) {
+        // Add the assessor display name to the assessment while we have easy
+        // access to the provisional grade data
+        const assessment = grade.rubric_assessments[0]
+        assessment.assessor_name = provisionalGraderDisplayNames[grade.provisional_grade_id]
+        if (!currentAssessmentsById[assessment.id]) {
+          provisionalAssessments.push(assessment)
+        }
+      }
+    })
+
+    if (provisionalAssessments.length > 0) {
+      if (!this.currentStudent.rubric_assessments) {
+        this.currentStudent.rubric_assessments = []
+      }
+
+      this.currentStudent.rubric_assessments = this.currentStudent.rubric_assessments.concat(provisionalAssessments)
+    }
+
+    if (anonymousGraders) {
+      this.currentStudent.rubric_assessments.sort((a, b) => natcompare.strings(a.anonymous_assessor_id, b.anonymous_assessor_id))
+    }
+  },
+
   showStudent: function(){
     $rightside_inner.scrollTo(0);
     if (this.currentStudent.submission_state == 'not_gradeable' && ENV.grading_role == "provisional_grader") {
@@ -1344,10 +1406,11 @@ EG = {
       $rightside_inner.show();
     }
     if (ENV.grading_role == "moderator") {
+      this.renderProvisionalGradeSelector({showingNewStudent: true})
+      this.setCurrentStudentRubricAssessments()
+
       this.current_prov_grade_index = null;
       this.removeModerationBarAndShowSubmission();
-
-      this.renderProvisionalGradeSelector({showingNewStudent: true})
 
       const selectedGrade = currentStudentProvisionalGrades().find(grade => grade.selected);
       if (selectedGrade) {
@@ -2136,34 +2199,55 @@ EG = {
     if (jsonData.rubric_association) {
       ENV.RUBRIC_ASSESSMENT.assessment_user_id = this.currentStudent[anonymizableId];
 
-      var assessmentsByMe = $.grep(EG.currentStudent.rubric_assessments, function(n,i){
-        return n.assessor_id === ENV.RUBRIC_ASSESSMENT.assessor_id;
-      });
-      var gradingAssessments = $.grep(EG.currentStudent.rubric_assessments, function(n,i){
-        return n.assessment_type == 'grading';
+      const isModerator = ENV.grading_role === 'moderator'
+      const selectMenuOptions = []
+
+      const assessmentsByMe = EG.currentStudent.rubric_assessments
+        .filter(assessment => assessmentBelongsToCurrentUser(assessment))
+      if (assessmentsByMe.length > 0) {
+        assessmentsByMe.forEach(assessment => {
+          const displayName = isModerator ? customProvisionalGraderLabel : assessment.assessor_name
+          selectMenuOptions.push({ id: assessment.id, name: displayName })
+        })
+      } else if (isModerator) {
+        // Moderators can create a custom assessment if they don't have one
+        selectMenuOptions.push({ id: '', name: customProvisionalGraderLabel })
+      }
+
+      const assessmentsByOthers = EG.currentStudent.rubric_assessments
+        .filter(assessment => !assessmentBelongsToCurrentUser(assessment))
+
+      assessmentsByOthers.forEach(assessment => {
+        // Display anonymous graders as "Grader 1 Rubric" (but don't use the
+        // "Rubric" suffix for named graders)
+        let displayName = assessment.assessor_name
+        if (anonymousGraders) {
+          displayName += ' Rubric'
+        }
+
+        selectMenuOptions.push({ id: assessment.id, name: displayName })
       });
 
       selectMenu.find("option").remove();
-      $.each(this.currentStudent.rubric_assessments, function(){
-        selectMenu.append(`<option value="${htmlEscape(this.id)}">${htmlEscape(this.assessor_name)}</option>`);
-      });
+      selectMenuOptions.forEach(option => {
+        selectMenu.append(`<option value="${htmlEscape(option.id)}">${htmlEscape(option.name)}</option>`)
+      })
 
-      //select the assessment that meets these rules:
-      // 1. the assessment by me
-      // 2. the assessment with assessment_type = 'grading'
-      var idToSelect = null;
-      if (gradingAssessments.length) {
-        idToSelect = gradingAssessments[0].id;
-      }
-      if (assessmentsByMe.length) {
-        idToSelect = assessmentsByMe[0].id;
-      }
-      if (idToSelect) {
-        selectMenu.val(idToSelect);
+      let idToSelect = ''
+      if (assessmentsByMe.length > 0) {
+        idToSelect = assessmentsByMe[0].id
+      } else {
+        const gradingAssessment = EG.currentStudent.rubric_assessments
+          .find(assessment => assessment.assessment_type === 'grading')
+
+        if (gradingAssessment) {
+          idToSelect = gradingAssessment.id
+        }
       }
 
-      // hide the select box if there is not >1 option
-      $("#rubric_assessments_list").showIf(selectMenu.find("option").length > 1);
+      selectMenu.val(idToSelect)
+      $("#rubric_assessments_list").showIf(isModerator || selectMenu.find("option").length > 1);
+
       handleSelectedRubricAssessmentChanged({validateEnteredData});
     }
   },

public/javascripts/speed_grader_helpers.js
@@ -26,6 +26,10 @@ export function setupIsAnonymous ({anonymize_students}) {
   return anonymize_students
 }
 
+export function setupAnonymousGraders ({anonymize_graders}) {
+  return anonymize_graders
+}
+
 export function setupAnonymizableId (isAnonymous) {
   return isAnonymous ? 'anonymous_id' : 'id'
 }
@@ -174,6 +178,7 @@ export function setupAnonymizableAuthorId (isAnonymous) {
     },
 
     setupIsAnonymous,
+    setupAnonymousGraders,
     setupAnonymizableId,
     setupAnonymizableUserId,
     setupAnonymizableStudentId,

spec/javascripts/jsx/speed_graderSpec.js
@@ -4447,4 +4447,250 @@ QUnit.module('SpeedGrader', function(suiteHooks) {
       })
     })
   })
+
+  QUnit.module('a moderated assignment with a rubric', hooks => {
+    let originalJsonData
+    let moderatorProvisionalGrade
+    let provisionalGraderProvisionalGrade
+    let otherGraderProvisionalGrade
+    let submission
+    let student
+    let testJsonData
+
+    hooks.beforeEach(() => {
+      fakeENV.setup({
+        ...ENV,
+        anonymous_identities: {
+          'aaaaa': { id: 'aaaaa', name: 'Grader 1' },
+          'bbbbb': { id: 'bbbbb', name: 'Grader 2' },
+          'zzzzz': { id: 'zzzzz', name: 'Grader 3' }
+        },
+        current_anonymous_id: 'zzzzz',
+        current_user_id: '1',
+        RUBRIC_ASSESSMENT: {
+          assessment_type: 'grading',
+          assessor_id: '1'
+        }
+      })
+
+      moderatorProvisionalGrade = {
+        provisional_grade_id: '1',
+        scorer_id: '1',
+        scorer_name: 'Urd',
+        readonly: true,
+        rubric_assessments: [
+          {
+            id: '1',
+            assessor_id: '1',
+            assessor_name: 'Urd',
+            data: [{ points: 2, criterion_id: '9' }]
+          }
+        ]
+      }
+
+      provisionalGraderProvisionalGrade = {
+        provisional_grade_id: '3',
+        scorer_id: '3',
+        scorer_name: 'Verdandi',
+        readonly: true,
+        rubric_assessments: [
+          {
+            id: '3',
+            assessor_id: '3',
+            assessor_name: 'Verdandi',
+            data: [{ points: 4, criterion_id: '9' }]
+          }
+        ]
+      }
+
+      otherGraderProvisionalGrade = {
+        provisional_grade_id: '2',
+        scorer_id: '2',
+        scorer_name: 'Skuld',
+        readonly: true,
+        rubric_assessments: [
+          {
+            id: '2',
+            assessor_id: '2',
+            assessor_name: 'Skuld',
+            data: [{ points: 4, criterion_id: '9' }]
+          }
+        ]
+      }
+
+      submission = {
+        user_id: '10',
+        provisional_grades: [moderatorProvisionalGrade, otherGraderProvisionalGrade, provisionalGraderProvisionalGrade]
+      }
+
+      student = {
+        id: '10',
+        name: 'Sextus Student',
+        rubric_assessments: []
+      }
+
+      testJsonData = {
+        context: {
+          students: [student],
+          enrollments: [{user_id: '10', workflow_state: 'active', course_setion_id: 1}],
+          active_course_sections: [{ id: 1, name: 'The Only Section' }],
+        },
+        submissions: [submission],
+        rubric_association: {},
+        anonymous_grader_ids: ['zzzzz', 'bbbbb', 'aaaaa']
+      }
+
+      originalJsonData = window.jsonData
+
+      setupFixtures(`
+        <div id="rubric_summary_holder">
+          <div id="rubric_assessments_list_and_edit_button_holder">
+            <span id="rubric_assessments_list">
+              <select id="rubric_assessments_select"></select>
+            </span>
+          </div>
+          <div id="rubric_summary_container">
+            <div class="button-container">
+              <div class="edit"></div>
+            </div>
+          </div>
+        </div>
+        <select id="rubric_assessments_select">
+        </select>
+        <div id="rubric_summary_container">
+        </div>
+      `)
+
+      const getFromCache = sinon.stub(JQuerySelectorCache.prototype, 'get');
+      getFromCache.withArgs('#rubric_full').returns($('#rubric_full'));
+      getFromCache.withArgs('#rubric_assessments_select').returns($('#rubric_assessments_select'));
+
+      sandbox.stub(SpeedGrader.EG, 'handleFragmentChanged')
+    })
+
+    hooks.afterEach(() => {
+      SpeedGrader.EG.handleFragmentChanged.restore()
+      JQuerySelectorCache.prototype.get.restore();
+
+      window.jsonData = originalJsonData
+      SpeedGrader.teardown()
+      window.location.hash = ''
+      teardownFixtures()
+    })
+
+    QUnit.module('when the viewer is a provisional grader', graderHooks => {
+      const finishSetup = () => {
+        SpeedGrader.setup()
+        window.jsonData = testJsonData
+        SpeedGrader.EG.jsonReady()
+        SpeedGrader.EG.currentStudent = student
+        SpeedGrader.EG.showRubric()
+      }
+
+      graderHooks.beforeEach(() => {
+        fakeENV.setup({
+          ...ENV,
+          current_anonymous_id: 'bbbbb',
+          current_user_id: '3',
+          grading_role: 'provisional_grader'
+        })
+      })
+
+      test('shows a button to edit the assessment', () => {
+        finishSetup()
+        ok($('#rubric_assessments_list_and_edit_button_holder .edit').is(':visible'))
+      })
+
+      test('hides the dropdown for selecting rubrics', () => {
+        finishSetup()
+        notOk($('#rubric_assessments_select').is(':visible'))
+      })
+    })
+
+    QUnit.module('when the viewer is a moderator', graderHooks => {
+      const finishSetup = () => {
+        SpeedGrader.setup()
+        window.jsonData = testJsonData
+        SpeedGrader.EG.jsonReady()
+        SpeedGrader.EG.currentStudent = testJsonData.context.students[0]
+        SpeedGrader.EG.setupProvisionalGraderDisplayNames()
+        SpeedGrader.EG.setCurrentStudentRubricAssessments()
+        SpeedGrader.EG.showRubric()
+      }
+
+      graderHooks.beforeEach(() => {
+        fakeENV.setup({
+          ...ENV,
+          grading_role: 'moderator'
+        })
+      })
+
+      test('shows all available rubrics in the dropdown', () => {
+        finishSetup()
+        strictEqual($('#rubric_assessments_select option').length, 3)
+      })
+
+      test('includes the moderator-submitted assessment at the top', () => {
+        finishSetup()
+        strictEqual($('#rubric_assessments_select option').first().val(), '1')
+      })
+
+      test('includes a blank assessment if the moderator has not submitted one', () => {
+        submission.provisional_grades = [otherGraderProvisionalGrade, provisionalGraderProvisionalGrade]
+
+        finishSetup()
+        strictEqual($('#rubric_assessments_select option:first').val(), '')
+      })
+
+      test('shows a button to edit if the assessment belonging to the moderator is selected', () => {
+        finishSetup()
+
+        $('#rubric_assessments_select').val('1').change()
+        ok($('#rubric_assessments_list_and_edit_button_holder .edit').is(':visible'))
+      })
+
+      test('does not show a button to edit if a different assessment is selected', () => {
+        finishSetup()
+
+        $('#rubric_assessments_select').val('3').change()
+        notOk($('#rubric_assessments_list_and_edit_button_holder .edit').is(':visible'))
+      })
+
+      test('labels the moderator-submitted assessment as "Custom"', () => {
+        finishSetup()
+        strictEqual($('#rubric_assessments_select option[value="1"]').text(), 'Custom')
+      })
+
+      test('appends "Rubric" to the grader name in the dropdown if graders are anonymous', () => {
+        testJsonData.anonymize_graders = true
+        moderatorProvisionalGrade.anonymous_grader_id = 'zzzzz'
+        moderatorProvisionalGrade.rubric_assessments[0].anonymous_assessor_id = 'zzzzz'
+        otherGraderProvisionalGrade.anonymous_grader_id = 'aaaaa'
+        otherGraderProvisionalGrade.rubric_assessments[0].anonymous_assessor_id = 'aaaaa'
+        provisionalGraderProvisionalGrade.anonymous_grader_id = 'bbbbb'
+        provisionalGraderProvisionalGrade.rubric_assessments[0].anonymous_assessor_id = 'bbbbb'
+
+        finishSetup()
+        strictEqual($('#rubric_assessments_select option[value="3"]').text(), 'Grader 2 Rubric')
+      })
+
+      test('does not append "Rubric" to the grader name in the dropdown if grader names are shown', () => {
+        finishSetup()
+        strictEqual($('#rubric_assessments_select option[value="3"]').text(), 'Verdandi')
+      })
+
+      test('defaults to the assessment by the moderator if there is one', () => {
+        finishSetup()
+        strictEqual($('#rubric_assessments_select').val(), '1')
+      })
+
+      test('defaults to the first assessment of type "grading" if the moderator does not have one', () => {
+        submission.provisional_grades = [otherGraderProvisionalGrade, provisionalGraderProvisionalGrade]
+        provisionalGraderProvisionalGrade.rubric_assessments[0].assessment_type = 'grading'
+
+        finishSetup()
+        strictEqual($('#rubric_assessments_select').val(), '3')
+      })
+    })
+  })
 })

app/controllers/gradebooks_controller.rb
@@ -657,6 +657,10 @@ class GradebooksController < ApplicationController
           env[:provisional_select_url] = api_v1_select_provisional_grade_path(@context.id, @assignment.id, "{{provisional_grade_id}}")
         end
 
+        unless @assignment.grades_published? || @assignment.can_view_other_grader_identities?(@current_user)
+          env[:current_anonymous_id] = @assignment.moderation_graders.find_by!(user_id: @current_user.id).anonymous_id
+        end
+
         if new_gradebook_enabled?
           env[:selected_section_id] = gradebook_settings.dig(@context.id, 'filter_rows_by', 'section_id')
         end

app/controllers/rubric_assessments_controller.rb
@@ -93,6 +93,11 @@ class RubricAssessmentsController < ApplicationController
             json[:artifact] = @asset.submission.
               as_json(Submission.json_serialization_full_parameters(include_root: false)).
               merge(@asset.grade_attributes)
+
+            if @association_object.moderated_grading? && !@association_object.can_view_other_grader_identities?(@current_user)
+              current_user_moderation_grader = @association_object.moderation_graders.find_by(user: @current_user)
+              json[:anonymous_assessor_id] = current_user_moderation_grader.anonymous_id
+            end
           end
 
           render json: json

app/models/assignment/speed_grader.rb
@@ -58,6 +58,7 @@ class Assignment
         :include_root => false
       )
       res['anonymize_students'] = @assignment.anonymize_students?
+      res['anonymize_graders'] = !@assignment.can_view_other_grader_identities?(@current_user)
 
       # include :provisional here someday if we need to distinguish
       # between provisional and real comments (also in

app/views/gradebooks/speed_grader.html.erb
@@ -274,7 +274,7 @@
               <div id="rubric_summary_holder">
                 <div id="rubric_assessments_list_and_edit_button_holder">
                   <span id="rubric_assessments_list">
-                    <%= before_label(:show_assessment_by, "Show Assessment By") %>
+                    <h2><%= t("Select Rubric") %></h2>
                     <select id="rubric_assessments_select"></select>
                   </span>
                 </div>

spec/coffeescripts/speed_grader/SpeedgraderHelpersSpec.js
@@ -19,6 +19,7 @@
 import fakeENV from 'helpers/fakeENV'
 import SpeedgraderHelpers, {
   setupIsAnonymous,
+  setupAnonymousGraders,
   setupAnonymizableId,
   setupAnonymizableUserId,
   setupAnonymizableStudentId,
@@ -481,6 +482,21 @@ QUnit.module('SpeedgraderHelpers.setupIsAnonymous', suiteHooks => {
 
   test('returns false when assignment has anonymize_students set to false', () => {
     strictEqual(setupIsAnonymous({anonymize_students: false}), false)
+  })
+})
+
+QUnit.module('SpeedgraderHelpers.setupAnonymousGraders', suiteHooks => {
+  suiteHooks.afterEach(() => {
+    fakeENV.teardown()
+  })
+
+  test('returns true when assignment has anonymize_graders set to true', () => {
+    strictEqual(setupAnonymousGraders({anonymize_graders: true}), true)
+    fakeENV.teardown()
+  })
+
+  test('returns false when assignment has anonymize_graders set to false', () => {
+    strictEqual(setupAnonymousGraders({anonymize_graders: false}), false)
   })
 })
 

commit 82fc286828a0ef105274f331d49767ca011d8d80

fix view rubric for teachers in speed grader
this bug was introduced in cfe2ce6

fixes: GRADE-1604

test plan:
 - given an assignment with an attached rubric
 - when a rubric assessment as a TA in Speed Grader
 - then a teacher can still leave a rubric assessment because the "View
   Rubric" button is still present

Change-Id: I6bc7e6343bfb1bbed5abccaeacc4f1bd47cda7e5
Reviewed-on: https://gerrit.instructure.com/164631
Reviewed-by: Keith T. Garner <kgarner@instructure.com>
Product-Review: Keith T. Garner <kgarner@instructure.com>
QA-Review: Keith T. Garner <kgarner@instructure.com>
Tested-by: Keith T. Garner <kgarner@instructure.com>

public/javascripts/speed_grader.js
@@ -50,7 +50,8 @@ import SpeedgraderHelpers, {
   setupAnonymizableUserId,
   setupAnonymizableAuthorId,
   setupAnonymousGraders,
-  setupIsAnonymous
+  setupIsAnonymous,
+  setupIsModerated
 } from './speed_grader_helpers';
 import turnitinInfoTemplate from 'jst/_turnitinInfo';
 import turnitinScoreTemplate from 'jst/_turnitinScore';
@@ -87,6 +88,7 @@ let anonymizableId
 let anonymizableUserId
 let anonymizableStudentId
 let anonymizableAuthorId
+let isModerated
 
 let $window
 let $full_width_container
@@ -836,7 +838,10 @@ function handleSelectedRubricAssessmentChanged({validateEnteredData = true} = {}
     editingData
   );
 
-  const showEditButton = !selectedAssessment || assessmentBelongsToCurrentUser(selectedAssessment)
+  let showEditButton = true
+  if (isModerated) {
+    showEditButton = !selectedAssessment || assessmentBelongsToCurrentUser(selectedAssessment)
+  }
   $('#rubric_assessments_list_and_edit_button_holder .edit').showIf(showEditButton)
 }
 
@@ -1160,6 +1165,7 @@ EG = {
 
   jsonReady: function(){
     isAnonymous = setupIsAnonymous(jsonData)
+    isModerated = setupIsModerated(jsonData)
     anonymousGraders = setupAnonymousGraders(jsonData)
     anonymizableId = setupAnonymizableId(isAnonymous)
     anonymizableUserId = setupAnonymizableUserId(isAnonymous)

public/javascripts/speed_grader_helpers.js
@@ -22,6 +22,10 @@ import I18n from 'i18n!gradebook'
 import './jquery.instructure_date_and_time'
 import './jquery.instructure_misc_helpers'
 
+export function setupIsModerated ({moderated_grading}) {
+  return moderated_grading
+}
+
 export function setupIsAnonymous ({anonymize_students}) {
   return anonymize_students
 }
@@ -177,6 +181,7 @@ export function setupAnonymizableAuthorId (isAnonymous) {
       window.location.reload();
     },
 
+    setupIsModerated,
     setupIsAnonymous,
     setupAnonymousGraders,
     setupAnonymizableId,

spec/javascripts/jsx/speed_graderSpec.js
@@ -4530,6 +4530,7 @@ QUnit.module('SpeedGrader', function(suiteHooks) {
       }
 
       testJsonData = {
+        moderated_grading: true,
         context: {
           students: [student],
           enrollments: [{user_id: '10', workflow_state: 'active', course_setion_id: 1}],
@@ -4654,6 +4655,19 @@ QUnit.module('SpeedGrader', function(suiteHooks) {
 
         $('#rubric_assessments_select').val('3').change()
         notOk($('#rubric_assessments_list_and_edit_button_holder .edit').is(':visible'))
+      })
+
+      test('shows a button to edit', () => {
+        finishSetup()
+
+        ok($('#rubric_assessments_list_and_edit_button_holder .edit').is(':visible'))
+      })
+
+      test('shows a button to edit if moderated_grading disabled', () => {
+        testJsonData.moderated_grading = false
+        finishSetup()
+
+        ok($('#rubric_assessments_list_and_edit_button_holder .edit').is(':visible'))
       })
 
       test('labels the moderator-submitted assessment as "Custom"', () => {