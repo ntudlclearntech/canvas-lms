fae394babbad647351338fe26b05a9ba5cdbb193
Shawn Li <tp9106@gmail.com>
2019-02-19 11:03:21 +0800

    [UI] apply tabs style for tos page

--- ui/features/terms_of_service_modal/react/TermsOfServiceModal.js
+++ ui/features/terms_of_service_modal/react/TermsOfServiceModal.js
@@ -81,6 +81,30 @@ export default class TermsOfServiceModal extends React.Component {
         TERMS_OF_SERVICE_CUSTOM_CONTENT,
       }
     })
+    this.applyTabs();
+  }
+
+  applyTabs = () => {
+    setTimeout(() => {
+      function onTabClick(event) {
+        let activeTabs = document.querySelectorAll('.cool-tos-nav .active, .cool-tos-tab-content .active');
+        
+        activeTabs.forEach(function(tab) {
+          tab.className = tab.className.replace('active', '');
+        });
+        
+        event.target.parentElement.className += ' active';
+        document.getElementById(event.target.href.split('#')[1]).className += ' active';
+        setTimeout(() => {
+          document.getElementById('cool-tos-back-to-top').click();
+        }, 0);
+      }
+      
+      const element = document.getElementById('cool-tos-nav-tab');
+      if (element) { 
+        element.addEventListener('click', onTabClick, false);
+      }
+    }, 300);
   }
 
   render() {
