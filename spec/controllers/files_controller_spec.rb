ntudlclearntech/canvas-lms

commit 680fbe5c27c3fe7ddeac20b9d8f9b76376af2b44

Implement Inst-fs capture callback for url uploads.
Adds an alert when the student submits stating that the upload is
processing and an email will be sent.

THe FilesController#api_capture called by InstFS, when the upload
processing is finished, uses Services::SubmitHomeworkService#submit
to submit the assignment.

Update the JS to prevent the browser from submitting the url upload
assignment twice. Passes the submit_assignment parameter to provide
backwards compatibilty, so the old JS can still double submit.

fixes: GOOF-449, GOOF-450, GOOF-435

Test Plan:
* Submit a file from the Google Drive, will see a pop up.
* Check the xhr for the request, the submit_assignment param will be true
* InstFS callback will cause the assignment to be submitted.

Change-Id: I5f49b8ef642fe5913bf0c2f282de8d697dcf1ce1
Reviewed-on: https://gerrit.instructure.com/154757
Reviewed-by: Nick Houle <nhoule@instructure.com>
QA-Review: Tyler Belnap <tyler@instructure.com>
Reviewed-by: Cody Cutrer <cody@instructure.com>
Product-Review: Jesse Poulos <jpoulos@instructure.com>
Tested-by: Jenkins
Reviewed-by: Josh Orr <jgorr@instructure.com>

app/coffeescripts/views/assignments/ExternalContentFileSubmissionView.coffee
@@ -17,11 +17,11 @@
 
 define [
   'jquery'
+  'axios'
   'i18n!assignments'
   'jst/assignments/ExternalContentHomeworkFileSubmissionView'
   './ExternalContentHomeworkSubmissionView'
-  'jsx/shared/upload_file'
-], ($, I18n, template, ExternalContentHomeworkSubmissionView, uploader) ->
+], ($, axios, I18n, template, ExternalContentHomeworkSubmissionView) ->
 
   class ExternalContentFileSubmissionView extends ExternalContentHomeworkSubmissionView
     template: template
@@ -30,25 +30,29 @@ define [
     submitHomework: =>
       @uploadFileFromUrl(@externalTool, @model)
 
-    submitAssignment: (attachment) =>
-      data =
-        submission:
-          submission_type: "online_upload"
-          file_ids: [ attachment.id ]
-          eula_agreement_timestamp: $('#eula_agreement_timestamp').val()
-        comment:
-          text_comment: @assignmentSubmission.get('comment')
-
-      submissionUrl = "/api/v1/courses/" + ENV.COURSE_ID + "/assignments/" + ENV.SUBMIT_ASSIGNMENT.ID + "/submissions"
-      $.ajaxJSON submissionUrl, "POST", data, @redirectSuccessfulAssignment, @disableLoader
-
-      return
-
-    redirectSuccessfulAssignment: (responseData) =>
+    reloadSuccessfulAssignment: (responseData) =>
       $(window).off('beforeunload') # remove alert message from being triggered
+      alert(
+        I18n.t(
+          "processing_submission",
+          "Canvas is currently processing your submission. You can safely navigate away from this page and we will email you when the submission has processed."
+        )
+      )
       window.location.reload()
       @loaderPromise.resolve()
       return
+
+    sendCallbackUrl: (responseData) =>
+      uploadUrl = responseData.data.upload_url
+      if uploadUrl
+        formData = new FormData
+        uploadParams = responseData.data.upload_params
+
+        if uploadParams
+          for key of uploadParams
+            formData.append(key, uploadParams[key])
+
+        axios.post(uploadUrl, formData)
 
     disableLoader: =>
       @loaderPromise.resolve()
@@ -63,18 +67,23 @@ define [
 
       @assignmentSubmission = modelData
       # build the params for submitting the assignment
+      # TODO: The `submit_assignment` param is used to help in backwards compat for fixing auto submissions,
+      # can be removed in the next release.
       preflightData =
         url: @assignmentSubmission.get('url')
         name: @assignmentSubmission.get('text')
         content_type: ''
+        submit_assignment: true
+        eula_agreement_timestamp: @assignmentSubmission.get('eula_agreement_timestamp')
 
       if ENV.SUBMIT_ASSIGNMENT.GROUP_ID_FOR_USER?
         preflightUrl = "/api/v1/groups/" + ENV.SUBMIT_ASSIGNMENT.GROUP_ID_FOR_USER + "/files"
       else
         preflightUrl = "/api/v1/courses/" + ENV.COURSE_ID + "/assignments/" + ENV.SUBMIT_ASSIGNMENT.ID + "/submissions/" + ENV.current_user_id + "/files"
 
-      uploader.uploadFile(preflightUrl, preflightData, null)
-        .then(@submitAssignment)
+      axios.post(preflightUrl, preflightData)
+        .then(@sendCallbackUrl)
+        .then(@reloadSuccessfulAssignment)
         .catch(@submissionFailure)
 
       @$el.disableWhileLoading @loaderPromise,

app/controllers/files_controller.rb
@@ -870,9 +870,34 @@ class FilesController < ApplicationController
     if params[:progress_id]
       progress = Progress.find(params[:progress_id])
 
-      json = { "id" => @attachment.id }
-      progress.set_results(json)
-      progress.complete!
+      # TODO: The `submit_assignment` param is used to help in backwards compat for fixing auto submissions,
+      # can be removed in the next release.
+      if params[:submit_assignment].to_s == 'true'
+        if progress.tag == 'upload_via_url'
+          assignment = progress.context
+          begin
+            Services::SubmitHomeworkService.submit(
+              @attachment,
+              assignment,
+              progress.created_at,
+              params[:eula_agreement_timestamp]
+            )
+            Services::SubmitHomeworkService.successful_email(@attachment, assignment)
+          rescue => error
+            error_id = Canvas::Errors.capture_exception(self.class.name, error)[:error_report]
+            progress.message = "Unexpected error, ID: #{error_id || 'unknown'}"
+            progress.save
+            progress.fail
+            logger.error "Error submitting a file: #{error} - #{error.backtrace}"
+            Services::SubmitHomeworkService.failure_email(@attachment, assignment)
+          end
+        end
+      end
+
+      if progress.running?
+        progress.set_results('id' => @attachment.id)
+        progress.complete!
+      end
     end
 
     url_params = { include: [] }

lib/api/v1/attachment.rb
@@ -223,11 +223,19 @@ module Api::V1::Attachment
     # no permission check required to use the preferred folder
 
     folder ||= opts[:folder]
+    progress_context = opts[:assignment] || @current_user
     if InstFS.enabled?
-      progress_json = if params[:url]
-        progress = ::Progress.new(context: @current_user, tag: :upload_via_url)
+      additional_capture_params = {}
+      progress_json_result = if params[:url]
+        progress = ::Progress.new(context: progress_context, user_id: @current_user, tag: :upload_via_url)
         progress.start
         progress.save!
+
+        additional_capture_params = {
+          submit_assignment: params[:submit_assignment],
+          eula_agreement_timestamp: params[:eula_agreement_timestamp]
+        }
+
         progress_json(progress, @current_user, session)
       end
 
@@ -244,8 +252,9 @@ module Api::V1::Attachment
         quota_exempt: !opts[:check_quota],
         capture_url: api_v1_files_capture_url,
         target_url: params[:url],
-        progress_json: progress_json,
-        include_param: params[:success_include]
+        progress_json: progress_json_result,
+        include_param: params[:success_include],
+        additional_capture_params: additional_capture_params
       )
     else
       @attachment = Attachment.new
@@ -263,7 +272,7 @@ module Api::V1::Attachment
 
       on_duplicate = infer_on_duplicate(params)
       if params[:url]
-        progress = ::Progress.new(context: @current_user, tag: :upload_via_url)
+        progress = ::Progress.new(context: progress_context, user: @current_user, tag: :upload_via_url)
         progress.reset!
         progress.process_job(
           @attachment,

lib/services/submit_homework_service.rb
@@ -1,0 +1,75 @@
+#
+# Copyright (C) 2016 - present Instructure, Inc.
+#
+# This file is part of Canvas.
+#
+# Canvas is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# Canvas is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Affero General Public License along
+# with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+
+module Services
+  class SubmitHomeworkService
+
+    EmailJob = Struct.new(:message) do
+      def perform
+        Mailer.deliver(Mailer.create_message(message))
+      end
+    end
+
+    class << self
+      def submit(attachment, assignment, submitted_at, eula_agreement_timestamp)
+        opts = {
+          submission_type: 'online_upload',
+          submitted_at: submitted_at,
+          attachments: [attachment],
+          eula_agreement_timestamp: eula_agreement_timestamp
+        }
+
+        assignment.submit_homework(attachment.user, opts)
+      end
+
+      def successful_email(attachment, assignment)
+        body = "Your file, #{attachment.display_name}, has been successfully "\
+               "uploaded to your Canvas assignment, #{assignment.name}"
+        user_email = User.find(attachment.user_id).email
+
+        message = OpenStruct.new(
+          from_name: 'notifications@instructure.com',
+          subject: "Submission upload successful: #{assignment.name}",
+          to: user_email,
+          body: body
+        )
+        deliver_email(message)
+      end
+
+      def failure_email(attachment, assignment)
+        body = "Your file, #{attachment.display_name}, failed to upload to your "\
+               "Canvas assignment, #{assignment.name}. Please re-submit to "\
+               "the assignment or contact your instructor if you are no "\
+               "longer able to do so."
+        user_email = User.where(id: attachment.user_id).first.email
+
+        message = OpenStruct.new(
+          from_name: 'notifications@instructure.com',
+          subject: "Submission upload failed: #{assignment.name}",
+          to: user_email,
+          body: body
+        )
+        deliver_email(message)
+      end
+
+      def deliver_email(message)
+        Delayed::Job.enqueue(EmailJob.new(message))
+      end
+    end
+  end
+end

spec/apis/v1/submissions_api_spec.rb
@@ -3823,10 +3823,23 @@ describe 'Submissions API', type: :request do
       include_examples "file uploads api"
       include_examples "file uploads api without quotas"
 
-      def preflight(preflight_params)
-        api_call(:post, "/api/v1/courses/#{@course.id}/assignments/#{@assignment.id}/submissions/#{@student1.id}/files",
-          { :controller => "submissions_api", :action => "create_file", :format => "json", :course_id => @course.to_param, :assignment_id => @assignment.to_param, :user_id => @student1.to_param },
-          preflight_params)
+      # preflight_params has to be first and nameless to keep backwards compat with the include_examples
+      def preflight(preflight_params, request_params: {}, api_url: nil)
+        api_url ||= "/api/v1/courses/#{@course.id}/assignments/#{@assignment.id}/submissions/#{@student1.id}/files"
+
+        api_call(
+          :post,
+          api_url,
+          {
+            controller: "submissions_api",
+            action: "create_file",
+            format: "json",
+            course_id: @course.to_param,
+            assignment_id: @assignment.to_param,
+            user_id: @student1.to_param,
+          }.merge(request_params),
+          preflight_params
+        )
       end
 
       def has_query_exemption?
@@ -3834,8 +3847,13 @@ describe 'Submissions API', type: :request do
       end
 
       it "rejects uploading files to other students' submissions" do
-        api_call(:post, "/api/v1/courses/#{@course.id}/assignments/#{@assignment.id}/submissions/#{@student2.id}/files",
-                        { :controller => "submissions_api", :action => "create_file", :format => "json", :course_id => @course.to_param, :assignment_id => @assignment.to_param, :user_id => @student2.to_param }, {}, {}, { :expected_status => 401 })
+        preflight(
+          {},
+          api_url: "/api/v1/courses/#{@course.id}/assignments/#{@assignment.id}/submissions/#{@student2.id}/files",
+          request_params: { user_id: @student2.to_param }
+        )
+
+        assert_status(401)
       end
 
       it "uploads to a student's Submissions folder" do
@@ -3844,25 +3862,73 @@ describe 'Submissions API', type: :request do
         expect(f.submission_context_code).to eq @course.asset_string
       end
 
-      context 'when InstFS is disabled' do
+      context 'for url upload using DelayedJob' do
+        let(:json_response) do
+          preflight(url: 'http://example.com/test', filename: 'test.txt')
+          JSON.parse(response.body)
+        end
+
         before { allow(InstFS).to receive(:enabled?).and_return(false) }
 
-        context "for a url submission" do
-          let(:url) { 'http://test.test/path' }
-          let(:result) { JSON.parse(response.body) }
+        it "returns progress json" do
+          progress_json = {
+            "context_id" => @assignment.id,
+            "context_type" =>"Assignment",
+            "user_id" => @student1.id,
+            "tag" => "upload_via_url"
+          }
+          expect(json_response['progress']).to include progress_json
+        end
+      end
 
-          before { preflight(name: 'test.txt', url: url) }
+      context 'for url upload using InstFS' do
+        let(:json_response) do
+          preflight(url: 'http://example.com/test')
+          JSON.parse(response.body)
+        end
 
-          it "creates a Progress" do
-            expect(Progress.find(result['progress']['id'])).to be
+        before { allow(InstFS).to receive(:enabled?).and_return(true) }
+
+        context 'returns an upload_url with a token' do
+          let(:token) { json_response['upload_url'].match(/\?token=(.+)&?/)[1] }
+          let(:jwt) { Canvas::Security.decode_jwt(token) }
+
+          it 'encodes capture_params in the token' do
+            capture_params = {
+              "submit_assignment" => nil,
+              "eula_agreement_timestamp" => nil,
+              "context_type" => "User",
+              "context_id" => @student1.id.to_s,
+              "user_id" => @student1.id.to_s,
+              "quota_exempt" => true,
+              "on_duplicate" => "overwrite",
+              "include" => nil
+            }
+            expect(jwt['capture_params']).to include capture_params
           end
 
-          it "creates a Progress worker" do
-            job = Delayed::Job.last
-            expect(job.tag).to eq 'Attachment#clone_url'
-            expect(job.priority).to eq Delayed::HIGH_PRIORITY
-            expect(job.handler).to match url
+          it "returns a valid upload url" do
+            expect(json_response['upload_url']).to match(/files\?token=.+&?/)
           end
+        end
+
+        it "returns upload_params" do
+          upload_json = {
+            "filename" => nil,
+            "content_type" => "unknown/unknown",
+            "target_url" => "http://example.com/test"
+          }
+          expect(json_response['upload_params']).to eq upload_json
+        end
+
+        it 'returns progress json' do
+          progress_json = {
+            "context_id" => @assignment.id,
+            "context_type" =>"Assignment",
+            "user_id" => @student1.id,
+            "tag" => "upload_via_url"
+          }
+          expect(json_response['progress']).to include progress_json
         end
       end
     end

commit 64f328505701c20639ac0e2d44c1a391f2d47836

Fix comments for external tool file submissions
Comments when submitting a file upload via an external tool appear to
have been broken in
https://github.com/instructure/canvas-lms/commit/680fbe5c27c3fe7ddeac

This commit changes the UI to again send the comment to the server, and
changes the server-side code to process the comment.

The server side code has two possible data paths:
* With InstFS:
  include the comment in the JWT we send (in capture_params) to InstFS.
  When InstFS is done it hits FilesController#api_capture. Now it
  include comment -- so we now take the comment and pass it into
  SubmitHomeworkService which submits it.
* Without InstFS:
  pass the comment in when making the asynchronous job in
  SubmitHomeworkService.submit_job() in this case, though.

Note: File uploads using an external tool appear to not work at all (?)
using Assignments 2. This fix is for "classic" Assignments.

Also, I can get file uploads using an external tool for group
assignments to work at all, locally or production. It was supposedly
working as of 8d69df44. Someone else should check and we can create
another ticket for that apparently separate issue. I did test that the
"comment" is now getting through to the call to SubmitHomeworkService.

flag=none
closes PLAT-4390

Test plan:
- If running locally, disable Assignments 2 by editing
  config/feature_flags/meeseeks_release_flags.yml and commenting out the
  hardcoded "environments: development: state: on" config under
  "assignments_2_student"
- You may need to run `node_modules/bin/.webpack` or `rails
  canvas:compile_assets` to compile the coffeescript.
- Install an LTI tool such as Box or Google Drive which allows file
  upload.
- Make an assignment that accepts file upload
- As a student, submit an assignment by choosing a file to upload with
  the LTI tool. After choosing a file, add a comment and submit.
- Start asynchronous jobs if running Canvas locally.

app/coffeescripts/views/assignments/ExternalContentFileSubmissionView.coffee
@@ -70,6 +70,7 @@ export default class ExternalContentFileSubmissionView extends ExternalContentHo
       name: @assignmentSubmission.get('text')
       content_type: ''
       eula_agreement_timestamp: @assignmentSubmission.get('eula_agreement_timestamp')
+      comment: @assignmentSubmission.get('comment')
 
     if ENV.SUBMIT_ASSIGNMENT.GROUP_ID_FOR_USER?
       preflightUrl = "/api/v1/groups/" + ENV.SUBMIT_ASSIGNMENT.GROUP_ID_FOR_USER + "/files?assignment_id=#{ENV.SUBMIT_ASSIGNMENT.ID}"

app/controllers/files_controller.rb
@@ -914,7 +914,7 @@ class FilesController < ApplicationController
         homework_service = Services::SubmitHomeworkService.new(@attachment, progress)
 
         begin
-          homework_service.submit(params[:eula_agreement_timestamp])
+          homework_service.submit(params[:eula_agreement_timestamp], params[:comment])
           homework_service.success!
         rescue => error
           error_id = Canvas::Errors.capture_exception(self.class.name, error)[:error_report]

lib/api/v1/attachment.rb
@@ -250,6 +250,7 @@ module Api::V1::Attachment
         if progress_context.is_a? Assignment
           additional_capture_params = {
             eula_agreement_timestamp: params[:eula_agreement_timestamp],
+            comment: params[:comment],
             submit_assignment: opts[:submit_assignment]
           }
         end
@@ -298,7 +299,7 @@ module Api::V1::Attachment
         )
 
         Services::SubmitHomeworkService.submit_job(
-          @attachment, progress, params[:eula_agreement_timestamp], executor, opts[:submit_assignment]
+          @attachment, progress, params[:eula_agreement_timestamp], params[:comment], executor, opts[:submit_assignment]
         )
 
         json = { progress: progress_json(progress, @current_user, session) }

lib/services/submit_homework_service.rb
@@ -36,7 +36,7 @@ module Services
       end
     end
 
-    CopyWorker = Struct.new(:attachment_id, :progress_id, :eula_agreement_timestamp, :clone_url_executor) do
+    CopyWorker = Struct.new(:attachment_id, :progress_id, :clone_url_executor) do
       def progress
         @progress ||= Progress.find(progress_id)
       end
@@ -70,7 +70,7 @@ module Services
       end
     end
 
-    SubmitWorker = Struct.new(:attachment_id, :progress_id, :eula_agreement_timestamp, :clone_url_executor) do
+    SubmitWorker = Struct.new(:attachment_id, :progress_id, :eula_agreement_timestamp, :comment, :clone_url_executor) do
       def progress
         @progress ||= Progress.find(progress_id)
       end
@@ -90,7 +90,7 @@ module Services
 
         raise(CloneUrlError, attachment.upload_error_message) if attachment.file_state == 'errored'
 
-        homework_service.submit(eula_agreement_timestamp)
+        homework_service.submit(eula_agreement_timestamp, comment)
         homework_service.success!
       rescue => error
         mark_as_failure(error)
@@ -112,14 +112,14 @@ module Services
         CloneUrlExecutor.new(url, duplicate_handling, check_quota, opts)
       end
 
-      def submit_job(attachment, progress, eula_agreement_timestamp, executor, submit_assignment)
+      def submit_job(attachment, progress, eula_agreement_timestamp, comment, executor, submit_assignment)
         if progress.context.is_a?(Assignment) && submit_assignment
           SubmitWorker.
-            new(attachment.id, progress.id, eula_agreement_timestamp, executor).
+            new(attachment.id, progress.id, eula_agreement_timestamp, comment, executor).
             tap { |worker| enqueue_attachment_job(worker) }
         else
           CopyWorker.
-            new(attachment.id, progress.id, eula_agreement_timestamp, executor).
+            new(attachment.id, progress.id, executor).
             tap { |worker| enqueue_attachment_job(worker) }
         end
       end
@@ -138,7 +138,7 @@ module Services
       @progress = progress
     end
 
-    def submit(eula_agreement_timestamp)
+    def submit(eula_agreement_timestamp, comment)
       start!
 
       if @attachment
@@ -146,7 +146,8 @@ module Services
           submission_type: 'online_upload',
           submitted_at: @progress.created_at,
           attachments: [@attachment],
-          eula_agreement_timestamp: eula_agreement_timestamp
+          eula_agreement_timestamp: eula_agreement_timestamp,
+          comment: comment
         }
 
         @progress.context.submit_homework(@progress.user, opts)

spec/apis/v1/submissions_api_spec.rb
@@ -4150,28 +4150,9 @@ describe 'Submissions API', type: :request do
         expect(f.submission_context_code).to eq @course.asset_string
       end
 
-      context 'for url upload using DelayedJob' do
-        let(:json_response) do
-          preflight(url: 'http://example.com/test', filename: 'test.txt')
-          JSON.parse(response.body)
-        end
-
-        before { allow(InstFS).to receive(:enabled?).and_return(false) }
-
-        it "returns progress json" do
-          progress_json = {
-            "context_id" => @assignment.id,
-            "context_type" =>"Assignment",
-            "user_id" => @student1.id,
-            "tag" => "upload_via_url"
-          }
-          expect(json_response['progress']).to include progress_json
-        end
-      end
-
       context 'for url upload using InstFS' do
         let(:json_response) do
-          preflight(url: 'http://example.com/test')
+          preflight(url: 'http://example.com/test', comment: 'my comment')
           JSON.parse(response.body)
         end
 
@@ -4184,6 +4165,7 @@ describe 'Submissions API', type: :request do
           it 'encodes capture_params in the token' do
             capture_params = {
               "eula_agreement_timestamp" => nil,
+              "comment" => "my comment",
               "context_type" => "User",
               "context_id" => @student1.id.to_s,
               "user_id" => @student1.id.to_s,
@@ -4221,7 +4203,7 @@ describe 'Submissions API', type: :request do
 
       context 'for url upload using DelayedJob' do
         let(:json_response) do
-          preflight(url: 'http://example.com/test', filename: 'test.txt')
+          preflight(url: 'http://example.com/test', filename: 'test.txt', comment: 'hello comment')
           JSON.parse(response.body)
         end
 
@@ -4241,6 +4223,7 @@ describe 'Submissions API', type: :request do
           json_response
           job = Delayed::Job.order(:id).last
           expect(job.handler).to include Services::SubmitHomeworkService::SubmitWorker.name
+          expect(job.handler).to include 'hello comment'
         end
 
         it 'should enqueue the copy job when the submit_assignment parameter is false' do

spec/controllers/files_controller_spec.rb
@@ -1352,10 +1352,12 @@ describe FilesController do
           let(:progress_params) do
             assignment_params.merge(
               progress_id: progress.id,
+              comment: comment,
               eula_agreement_timestamp: eula_agreement_timestamp
             )
           end
           let(:eula_agreement_timestamp) { '1522419910' }
+          let(:comment) { 'my assignment comment' }
           let(:request) { post "api_capture", params: progress_params }
 
           before do
@@ -1363,12 +1365,12 @@ describe FilesController do
           end
 
           it 'should submit the attachment if the submit_assignment flag is not provided' do
-            expect(homework_service).to receive(:submit).with(eula_agreement_timestamp)
+            expect(homework_service).to receive(:submit).with(eula_agreement_timestamp, comment)
             request
           end
 
           it 'should submit the attachment if the submit_assignment param is set to true' do
-            expect(homework_service).to receive(:submit).with(eula_agreement_timestamp)
+            expect(homework_service).to receive(:submit).with(eula_agreement_timestamp, comment)
             post "api_capture", params: progress_params.merge(submit_assignment: true)
           end
 
@@ -1381,6 +1383,12 @@ describe FilesController do
             request
             submission = Submission.where(assignment_id: assignment.id)
             expect(submission.first.turnitin_data[:eula_agreement_timestamp]).to eq(eula_agreement_timestamp)
+          end
+
+          it 'should save the comment' do
+            request
+            submission = Submission.where(assignment_id: assignment.id)
+            expect(submission.first.submission_comments.first.comment).to eq(comment)
           end
 
           it "returns a 201 http status" do

spec/javascripts/jsx/views/assignments/ExternalContentFileSubmissionViewSpec.js
@@ -77,3 +77,10 @@ test('sends the eula agreement timestamp to the submission endpoint', () => {
   equal(spy.args[0][1].eula_agreement_timestamp, model.get('eula_agreement_timestamp'))
   ok(spy.calledWith('/api/v1/courses/42/assignments/24/submissions/5/files'))
 })
+
+test('sends the comment to the submission endpoint', () => {
+  const spy = sandbox.spy(axios, 'post')
+  view.uploadFileFromUrl({}, model)
+  equal(spy.args[0][1].comment, model.get('comment'))
+  ok(spy.calledWith('/api/v1/courses/42/assignments/24/submissions/5/files'))
+})

spec/lib/services/submit_homework_service_spec.rb
@@ -46,6 +46,7 @@ module Services
       )
     end
     let(:eula_agreement_timestamp) { "1522419910" }
+    let(:comment) { "what a comment" }
     let(:url) { 'url' }
     let(:dup_handling) { false }
     let(:check_quota) { false }
@@ -75,7 +76,7 @@ module Services
     describe '.submit_job' do
       let(:service) { described_class.new(attachment, progress) }
       let(:worker) do
-        described_class.submit_job(attachment, progress, eula_agreement_timestamp, executor, submit_assignment)
+        described_class.submit_job(attachment, progress, eula_agreement_timestamp, comment, executor, submit_assignment)
       end
 
       before do
@@ -85,14 +86,14 @@ module Services
 
       it 'should clone and submit the url when submit_assignment is true' do
         expect(attachment).to receive(:clone_url).with(url, dup_handling, check_quota, opts)
-        expect(service).to receive(:submit).with(eula_agreement_timestamp)
+        expect(service).to receive(:submit).with(eula_agreement_timestamp, comment)
         worker.perform
 
         expect(progress.reload.workflow_state).to eq 'completed'
       end
 
       it 'should clone and not submit the url when submit_assignment is false' do
-        worker = described_class.submit_job(attachment, progress, eula_agreement_timestamp, executor, false)
+        worker = described_class.submit_job(attachment, progress, eula_agreement_timestamp, comment, executor, false)
         allow(worker).to receive(:homework_service).and_return(service)
         allow(worker).to receive(:attachment).and_return(attachment)
         expect(attachment).to receive(:clone_url).with(url, dup_handling, check_quota, opts)
@@ -135,7 +136,7 @@ module Services
     end
 
     describe '#submit' do
-      let(:submitted) { subject.submit(eula_agreement_timestamp) }
+      let(:submitted) { subject.submit(eula_agreement_timestamp, comment) }
       let(:recent_assignment) { assignment.reload }
 
       it 'should set submitted_at to the Progress#created_at' do
@@ -148,6 +149,10 @@ module Services
 
       it 'should set assignment for the submission' do
         expect(submitted.assignment).to eq recent_assignment
+      end
+
+      it 'should submit with the comment' do
+        expect(submitted.submission_comments.first.comment).to eq(comment)
       end
 
       it 'is a successful upload' do