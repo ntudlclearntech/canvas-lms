f8a17ae18fdbc2767391723e9e3287cfc3718929
kekmeepo3 <kekmeepo3@gmail.com>
2021-06-25 10:35:23 +0800

    Fix translations #112

--- ui/features/gradebook_uploads/jquery/index.js
+++ ui/features/gradebook_uploads/jquery/index.js
@@ -109,7 +109,7 @@ const GradebookUploader = {
       const newGrade = {
         id: this.id,
         type: 'assignments',
-        name: htmlEscape(I18n.t('To')),
+        name: htmlEscape(I18n.t('To', 'To')),
         field: this.id,
         width: 125,
         editor: Slick.Editors.UploadGradeCellEditor,
@@ -131,7 +131,7 @@ const GradebookUploader = {
         width: 125,
         formatter: GradebookUploader.createNumberFormatter('original_grade'),
         field: `${this.id}_conflicting`,
-        name: htmlEscape(I18n.t('From')),
+        name: htmlEscape(I18n.t('From', 'From')),
         cssClass: 'conflicting-grade'
       }
 
@@ -152,7 +152,7 @@ const GradebookUploader = {
         id: `custom_col_${column.id}`,
         customColumnId: column.id,
         type: 'custom_column',
-        name: htmlEscape(I18n.t('To')),
+        name: htmlEscape(I18n.t('To', 'To')),
         field: `custom_col_${column.id}`,
         width: 125,
         editor: Slick.Editors.UploadGradeCellEditor,
@@ -168,7 +168,7 @@ const GradebookUploader = {
         width: 125,
         formatter: GradebookUploader.createGeneralFormatter('current_content'),
         field: `custom_col_${column.id}_conflicting`,
-        name: htmlEscape(I18n.t('From')),
+        name: htmlEscape(I18n.t('From', 'From')),
         cssClass: 'conflicting-grade'
       }
 
@@ -497,7 +497,7 @@ const GradebookUploader = {
     const newOverrideScoreColumn = {
       id: `override_score_${id}`,
       type: 'assignment',
-      name: htmlEscape(I18n.t('To')),
+      name: htmlEscape(I18n.t('To', 'To')),
       field: `override_score_${id}`,
       width: 125,
       editor: Slick.Editors.UploadGradeCellEditor,
@@ -513,7 +513,7 @@ const GradebookUploader = {
       width: 125,
       formatter: GradebookUploader.createNumberFormatter('current_score'),
       field: `override_score_${id}_conflicting`,
-      name: htmlEscape(I18n.t('From')),
+      name: htmlEscape(I18n.t('From', 'From')),
       cssClass: 'conflicting-grade'
     }
     gridData.columns.push(conflictingOverrideScoreColumn, newOverrideScoreColumn)
